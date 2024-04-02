2ac623e50e783e1e3804952c9cd71cb2f1f177bb
zz19990220 <116690246+zz19990220@users.noreply.github.com>
2024-04-02 14:04:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ with col1:
         </style>
 
         <div class="profile-img">
-            <img src="https://pin.it/5RxXshJOK" alt="Profile Image">
+            <img src="https://images.unsplash.com/photo-1519256155806-cd510524ed97?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Profile Image">
         </div>
         """,
         unsafe_allow_html=True,
